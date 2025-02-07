26181a18d2f19fcf31eddad734b5d5a057415cbb
ahsanfaraz-WebDev <ahsanfaraza674@gmail.com>
2025-02-07 19:07:40 +0500

    Resolved merge conflicts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 
 /**
@@ -7,7 +8,7 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-
+ // Use ESM import
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
