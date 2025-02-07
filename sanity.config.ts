771158d6911452b841fa41e058d9cbb3521d859a
ahsanfaraz-WebDev <ahsanfaraza674@gmail.com>
2025-02-07 20:46:42 +0500

    Resolved merge conflicts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,7 @@
-"use client";
 
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
  */
-
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
@@ -13,7 +11,6 @@ import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schemaTypes";
 import { structure } from "./sanity/structure";
 import { markdownSchema } from "sanity-plugin-markdown";
-
 export default defineConfig({
   basePath: "/studio",
   projectId,
