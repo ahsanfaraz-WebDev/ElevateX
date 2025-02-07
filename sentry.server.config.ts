a6a326a16cbab5febc7fe783e9e522c26cf6fa74
ahsanfaraz-WebDev <ahsanfaraza674@gmail.com>
2025-02-07 17:23:44 +0500

    first commit

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5c7c1a9f0f8cc7fbd6ee2f9ed5733d5d@o4508773841240064.ingest.us.sentry.io/4508773855330304",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,52 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sanity/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "475px",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          "100": "#D6EAF8", // Light Soft Blue
+          DEFAULT: "#2471A3", // Deep Ocean Blue
+        },
+        secondary: "#F39C12", // Warm Golden Yellow
+        black: {
+          "100": "#2C3E50", // Dark Blue-Gray
+          "200": "#1C2833", // Deep Charcoal
+          "300": "#95A5A6", // Muted Gray
+          DEFAULT: "#000000", // Pure Black
+        },
+        white: {
+          "100": "#ECF0F1", // Soft Cool White
+          DEFAULT: "#FFFFFF", // Pure White
+        },
+      },
+      fontFamily: {
+        "work-sans": ["var(--font-work-sans)"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        100: "2px 2px 0px 0px rgb(0, 0, 0)",
+        200: "2px 2px 0px 2px rgb(0, 0, 0)",
+        300: "2px 2px 0px 2px #2471A3",
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
+
+export default config;
